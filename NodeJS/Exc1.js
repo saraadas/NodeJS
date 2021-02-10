c484596a5a53ed119297578eea00bc9d61c242f1
Sara Adas <sara.adaass@gmail.com>
2021-02-10 15:31:35 +0200

    Exc1

---
+++ NodeJS/Exc1.js
@@ -0,0 +1,143 @@
+// 1
+let x1;
+console.log(x1);
+
+// 2
+let x2=1;
+console.log(x2);
+
+// 3
+let x3,y3,z3;
+console.log(x3,y3,z3);
+
+// 4
+let x4 = 2,y4 = 3,z4 = 4;
+console.log(x4,y4,z4);
+
+// 5
+let x5 = true;
+console.log(x5);
+
+// 6
+const x6 = 10;
+console.log(x6);
+
+// 7
+let arr7 = [];
+console.log(arr7);
+
+// 8
+let arr8 = [1,2,3,4,5,6,7,8,9,10];
+console.log(arr8);
+
+// 9
+let arr9= ['A','B','C','D','E','F','G','H','I','J'];
+console.log(arr9);
+
+// 10
+let user10 = {
+    x: 1,
+    y: 2,
+    city: 3
+}
+console.log(user10);
+
+// 11
+let user11 = {
+    name: "Laith",
+    age: 25,
+    city: 'Amman'
+}
+console.log(user11);
+
+// 12
+let user12 = {
+    name: true,
+    age: false,
+    city: true
+}
+console.log(user12);
+
+// 13
+let user13 = {
+    key0: [1,2],
+    key1: ['A','B'],
+    key2: []
+}
+console.log(user13);
+
+// 14
+let user14 = [
+    {a: 1, b: 2},
+    {a: 3, b: 4},
+    {a: 5, b: 6},
+
+];
+console.log(user14);
+
+// 15
+let user15 =  {A: 'A', B: 4, C: true, D: [1], E: "Mars!" }
+console.log(user15);
+
+// 16
+let user16 = [
+    {a: 'A', b: 'B'},
+    {a: 3, b: 4},
+    {a: true, b: false},
+    {a: [1], b: [2]},
+    {a: "Mars", b: "Venus"},
+
+];
+console.log(user16);
+
+// 17
+laith = "Dab";
+let user17 = [
+    {a: [10], b: [20]},
+    {a: [30], b: [40]},
+    {a: [50], b: [60]},
+    {a: [70], b: [80]},
+    {a: [90], b: [100]},
+    {a: laith},
+
+];
+console.log(user17);
+
+// 18
+function sum(num1,num2){
+    return num1+num2;
+}
+console.log(sum(50,50));
+
+// 19
+function multiply1(num1,num2){
+    return num1*num2;
+}
+console.log(multiply(12,12));
+
+// 20
+let num1=0;
+let num2=0;
+function sum1(num1,num2){
+    return num1+num2;
+}
+console.log(sum1(50,50));
+
+// 21
+let num11=0;
+let num21=0;
+function multiply(num11,num21){
+    return num1*num2;
+}
+console.log(multiply(12,12));
+
+// 22
+function subtract(num1,num2){
+    return num1-num2;
+}
+console.log(subtract(550,50));
+
+// 23
+function sum2(num3,num4){
+    return num3+num4;
+}
